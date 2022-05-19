c8643c1f1f9132a49260bc309fb7d26e57d024cf
Eric Chelmu <eric.chelmu12@gmail.com>
2022-05-19 11:44:51 +0200

    Creature teleporting complete

--- Exurbia/Assets/Scripts/CreatureBehaviour.cs
+++ Exurbia/Assets/Scripts/CreatureBehaviour.cs
@@ -10,23 +10,38 @@ public class CreatureBehaviour : MonoBehaviour
     private float timer = 0;
     private float playerX;
     private float playerZ;
+    private float minTime = 5;
+    private float maxTime = 10;
+    private float spawnTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetRandomSpawnTime();
+        timer = minTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer <= 10f)
+        Debug.Log(timer);
+        timer += Time.deltaTime;
+        if (timer >= spawnTime -1)
         {
-            timer += Time.deltaTime;
+            Destroy(CreatureClone);
         }
-        else
+        if (timer >= spawnTime)
         {
-            timer = 0;
-            CreatureClone = Instantiate(Creature, new Vector3(PlayerScript.playerX + Random.Range(5, 15), 2.46f, PlayerScript.playerZ + Random.Range(5, 15)), PlayerScript.transform.rotation);
+            SpawnCreature();
+            SetRandomSpawnTime();
         }
+    }
+    void SpawnCreature()
+    {
+        timer = 0;
+        CreatureClone = Instantiate(Creature, new Vector3(PlayerScript.playerX + Random.Range(-30, 30), 2.46f, PlayerScript.playerZ + Random.Range(-30, 30)), PlayerScript.transform.rotation);
+    }
+    void SetRandomSpawnTime()
+    {
+        spawnTime = Random.Range(minTime, maxTime);
     }
 }
